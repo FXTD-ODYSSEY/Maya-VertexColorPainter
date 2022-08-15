73276adc01b64f80245bc1f108d44ac97695ef25
timmyliang <timmyliang@tencent.com>
2022-08-15 16:34:26 +0800

    fix: use setVertexColor API

--- plug-ins/verter_color_painter.py
+++ plug-ins/verter_color_painter.py
@@ -88,7 +88,6 @@ class AppVertexColorFilter(QtCore.QObject):
     pressed = QtCore.Signal()
     released = QtCore.Signal()
     property_showed = QtCore.Signal()
-    CHANNELS = "RGBA"
     inst = None
 
     channel_color_config = {
@@ -106,6 +105,9 @@ class AppVertexColorFilter(QtCore.QObject):
     }
 
     SINGLE_CONTROL = "artAttrColorSingleColorChannel"
+    OPTION_CONTROL = "artAttrColorSingleColorOptionMenu"
+    OPTION_ITEMS = ["Auto", "RGB", "R", "G", "B", "A"]
+    CHANNELS = "RGBA"
 
     def __init__(self, *args, **kwargs):
         super(AppVertexColorFilter, self).__init__(*args, **kwargs)
@@ -121,47 +123,39 @@ class AppVertexColorFilter(QtCore.QObject):
         for node in set(pm.artAttrPaintVertexCtx(PAINT_CTX, q=1, pna=1).split()):
             yield pm.PyNode(node)
 
+    @staticmethod
+    def get_color_sets(node):
+        color_sets = pm.polyColorSet(node, q=1, allColorSets=1)
+        return color_sets or pm.polyColorSet(node, create=1)
+
     @classmethod
     def setup_color_set(cls):
         for node in cls.get_paint_nodes():
-            print(node)
             node.displayColors.set(1)
-            color_sets = pm.polyColorSet(node, q=1, allColorSets=1)
-            color_sets = color_sets or pm.polyColorSet(node, create=1)
+            color_sets = cls.get_color_sets(node)
             main_color_set = color_sets[0]
 
-            for color_channel in cls.CHANNELS:
+            dag_path = node.__apimdagpath__()
+            mesh = OpenMaya.MFnMesh(dag_path)
+            color_array = OpenMaya.MColorArray()
+            mesh.getVertexColors(color_array, main_color_set)
+
+            for color_index, color_channel in enumerate(cls.CHANNELS):
                 color_set = "VertexColor{0}".format(color_channel)
                 if color_set not in color_sets:
                     rpt = cls.color_set_representation.get(color_channel)
                     pm.polyColorSet(node, create=1, rpt=rpt, colorSet=color_set)
-            pm.polyColorSet(node, currentColorSet=1, colorSet=main_color_set)
-
-            # NOTE(timmyliang): extract main color set to color channel set
-            dag_path = node.__apimdagpath__()
-            mesh = OpenMaya.MFnMesh(dag_path)
-            color_array = OpenMaya.MColorArray()
-            mesh.getColors(color_array, main_color_set)
-            channel_color_array = OpenMaya.MColorArray()
-
-            int_array = OpenMaya.MIntArray()
-            for itr in iterate_mit(OpenMaya.MItMeshFaceVertex(dag_path)):
-                int_array.append(itr.faceVertId())
-
-            for color_index, color_channel in enumerate(cls.CHANNELS):
-                color_set = "VertexColor{0}".format(color_channel)
-                channel_color_array.clear()
+                mesh.setCurrentColorSetName(color_set)
                 for array_index in range(color_array.length()):
                     full_color = color_array[array_index]
                     color = cls.channel_color_config[color_index](full_color)
-                    channel_color_array.append(color)
-                mesh.setColors(channel_color_array, color_set)
-                mesh.assignColors(int_array, color_set)
+                    mesh.setVertexColor(color, array_index)
+            mesh.setCurrentColorSetName(main_color_set)
 
     @classmethod
     def reset_color_set(cls):
         for node in cls.get_paint_nodes():
-            color_sets = pm.polyColorSet(node, q=1, allColorSets=1)
+            color_sets = cls.get_color_sets(node)
             for color_channel in cls.CHANNELS:
                 color_set = "VertexColor{0}".format(color_channel)
                 if color_set in color_sets:
@@ -232,7 +226,6 @@ class AppVertexColorFilter(QtCore.QObject):
         # TODO(timmyliang): get color channel apply to main color set
 
     def modify_property_window(self):
-        print("modify_ui")
         layout = pm.radioButtonGrp("artAttrColorChannelChoices", q=1, parent=1)
         pm.setParent(layout)
         pm.setUITemplate("OptionsTemplate", pushTemplate=1)
@@ -240,17 +233,44 @@ class AppVertexColorFilter(QtCore.QObject):
 
         for child in pm.layout(layout, q=1, childArray=1):
             pm.control(child, e=1, p=column_layout)
-            # NOTES(timmyliang): insert new radioButtonGrp
+            # NOTES(timmyliang): insert new ui
             if child == "artAttrColorChannelChoices":
                 pm.radioButtonGrp(
                     self.SINGLE_CONTROL,
                     label="Single Channel:",
                     nrb=4,
                     sl=1,
-                    la4=["All", "R", "G", "B"],
+                    la4=self.OPTION_ITEMS[1:-1],
+                    changeCommand=self.on_channel_change,
                 )
+                pm.optionMenuGrp(
+                    self.OPTION_CONTROL,
+                    label="Color Display:",
+                    changeCommand=self.on_display_mode_change,
+                )
+                for item in self.OPTION_ITEMS:
+                    pm.menuItem(label=item)
 
         pm.setUITemplate(popTemplate=1)
+
+    def on_channel_change(self, *args):
+        if pm.optionMenuGrp(self.OPTION_CONTROL, q=1, sl=1) == 1:
+            index = pm.radioButtonGrp(self.SINGLE_CONTROL, q=1, sl=1)
+            self.on_display_mode_change(self.OPTION_ITEMS[index])
+
+    @classmethod
+    def on_display_mode_change(cls, item):
+        if item == "Auto":
+            sel = pm.radioButtonGrp(cls.SINGLE_CONTROL, q=1, sl=1)
+            item = cls.OPTION_ITEMS[sel]
+        for node in cls.get_paint_nodes():
+            color_sets = cls.get_color_sets(node)
+            main_color_set = color_sets[0]
+            if item == "RGB":
+                pm.polyColorSet(node, currentColorSet=1, colorSet=main_color_set)
+            else:
+                color_set = "VertexColor{0}".format(item)
+                pm.polyColorSet(node, currentColorSet=1, colorSet=color_set)
 
 
 def mel_proc(func):
